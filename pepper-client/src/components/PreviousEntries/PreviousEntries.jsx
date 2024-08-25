f22c2fc237ee9be16b4694e23fe4b6f8bb6a7335
Delbin George <imdelbingeorge@gmail.com>
2024-08-25 13:26:42 +0530

    Trying to resolve conflict in build

--- pepper-client/src/components/PreviousEntries/PreviousEntries.jsx
+++ pepper-client/src/components/PreviousEntries/PreviousEntries.jsx
@@ -1,9 +1,25 @@
 import { useRecoilValue } from "recoil";
 import { userTransactions } from "../../provider/RecoilStore";
-import { categoryImages } from "../../assets/data/categoryImages.js";
 
 const PreviousEntries = () => {
     const transactions = useRecoilValue(userTransactions)
+
+    const categoryImages = {
+        gifts: "src/assets/images/icons/income-icon/gifts-icon.png",
+        salary: "src/assets/images/icons/income-icon/salary.png",
+        savings: "src/assets/images/icons/income-icon/savings.png",
+        others: "src/assets/images/icons/income-icon/other.png",
+        food: "src/assets/images/icons/expense-icon/food.png",
+        grocery: "src/assets/images/icons/expense-icon/grocery.png",
+        fuel: "src/assets/images/icons/expense-icon/fuel.png",
+        health: "src/assets/images/icons/expense-icon/health.png",
+        shopping: "src/assets/images/icons/expense-icon/shopping.png",
+        "debt paid": "src/assets/images/icons/expense-icon/debt-pay.png",
+        education: "src/assets/images/icons/expense-icon/education.png",
+        entertainment: "src/assets/images/icons/expense-icon/entertainment.png",
+        rent: "src/assets/images/icons/expense-icon/rent.png",
+        transport: "src/assets/images/icons/expense-icon/transport.png",
+    };
 
     return (
         <div className="transactions">
